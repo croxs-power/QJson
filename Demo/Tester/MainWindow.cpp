1c08257dfce1e37a12796444b893d725091fe066
croxs-power <liurui20080801@163.com>
2020-06-27 15:43:49 +0800

    change demon

--- Demo/Tester/MainWindow.cpp
+++ Demo/Tester/MainWindow.cpp
@@ -22,8 +22,38 @@ void MainWindow::Test()
     //{\"string\":\"hello world\",\"string_zh\":\"英文乱码\",\"double\":12.32321,\"int\":456,\"bool\":true}
     qDebug() << obj.format_string();
 
+    //
     json_object obj1;
     obj1.insert("name", "obj1");
+
+    //
+    json_array array;
+    array.append(1);
+    array.append(true);
+    array.append("string");
+    array.append(tr("中文乱码"));
+    array.append(12.324);
+    array.append(obj1);
+    qDebug() << array.format_string();
+
+    //
     obj.insert("obj", obj1);
+    obj.insert("array", array);
+    //{"string":"hello world","string_zh":"英文乱码","double":12.32321,"int":456,"bool":true,"obj":{"name":"obj1"}}
     qDebug() << obj.format_string();
+
+    //
+    qDebug() << obj.value("int").toInt() << obj.value("string_zh").toString();
+    json_object obj2 = obj.object("obj");
+    if(obj2)
+    {
+        qDebug() << obj2.value("name").toString();
+    }
+    json_array array1 = obj.array("array");
+    if(array1)
+    {
+        for(int i = 0; i < array1.size() - 1; i++)
+            qDebug() << array1.value(i).toString();
+        qDebug() << array1.object(5).format_string();
+    }
 }
